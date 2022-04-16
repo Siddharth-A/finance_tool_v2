cf540e0e86694e7a66b8e74dc3ac41f7d186c9a6
Siddharth-A <siddharth.ahluwalia16@gmail.com>
2022-04-15 23:41:17 -0400

    Complete construct_monthly_transactions
    - this function creates an aggregate transaction sheet
    - also add check for empty cells in cibc-chq and cibc-visa
    - replace empty cells with float 0

--- process_transactions.py
+++ process_transactions.py
@@ -36,7 +36,7 @@ cibc_csv_visa_sheet_name= ""
 # template_file_sheet_name = ""
 
 output_file             = tran_mon + "-transactions.xlsx"
-output_file_sheet_title = ""
+output_file_sheet_title = tran_mon + "-aggregate"
 
 wb = Workbook()
 ws = wb.active
@@ -63,15 +63,15 @@ bmo_mc sheet composition:
 - col D: credit transaction
 - col E: transaction type
 ###################################################"""
-def process_bmo_mc(bmo_csv_mc):
-    print("\n1) process BMO transactions file: {}".format(bmo_csv_mc))
+def process_bmo_mc(input_file):
+    print("\n1) process BMO transactions file: {}".format(input_file))
     wb = load_workbook(output_file)
     global bmo_csv_mc_sheet_name
     bmo_csv_mc_sheet_name = tran_mon + "-bmo mc"
     ws = wb.create_sheet(bmo_csv_mc_sheet_name)
 
     # copy from csv to output_file
-    with open(bmo_csv_mc,'r') as f:
+    with open(input_file,'r') as f:
         for row in csv.reader(f):
             ws.append(row)
 
@@ -129,7 +129,6 @@ def process_bmo_mc(bmo_csv_mc):
         ws.cell(row=i, column=5).value = 'BMO MC'
         i +=1
 
-
     # save workbook
     wb.save(output_file)
     print("done")
@@ -143,24 +142,32 @@ cibc chq sheet composition:
 - col D: credit transaction
 - col E: transaction type
 ###################################################"""
-def process_cibc_chq(cibc_csv_chq):
-    print("\n2) process CIBC CHQ transactions file: {}".format(cibc_csv_chq))
+def process_cibc_chq(input_file):
+    print("\n2) process CIBC CHQ transactions file: {}".format(input_file))
     wb = load_workbook(output_file)
     global cibc_csv_chq_sheet_name
     cibc_csv_chq_sheet_name = tran_mon + "-cibc chq"
     ws = wb.create_sheet(cibc_csv_chq_sheet_name)
 
     # copy from csv to output_file
-    with open(cibc_csv_chq,'r') as f:
+    with open(input_file,'r') as f:
         for row in csv.reader(f):
             ws.append(row)
 
     # format column 3 & 4 currency + add transaction type
     i = 1
     while i <= ws.max_row:
+
+        if ws.cell(row=i, column=3).value is "":
+            ws.cell(row=i, column=3).value = 0
+        
+        if ws.cell(row=i, column=4).value is "":
+            ws.cell(row=i, column=4).value = 0
+
         ws.cell(row=i, column=3).value = float(ws.cell(row=i, column=3).value)
-        ws.cell(row=i, column=3).number_format = numbers.FORMAT_CURRENCY_USD_SIMPLE
         ws.cell(row=i, column=4).value = float(ws.cell(row=i, column=4).value)
+
+        ws.cell(row=i, column=3).number_format = numbers.FORMAT_CURRENCY_USD_SIMPLE
         ws.cell(row=i, column=4).number_format = numbers.FORMAT_CURRENCY_USD_SIMPLE
         ws.cell(row=i, column=5).value = 'CIBC CHQ'
         i +=1
@@ -178,23 +185,34 @@ cibc visa sheet composition:
 - col D: credit transaction
 - col E: transaction type
 ###################################################"""
-def process_cibc_visa(cibc_csv_visa):
-    print("\n3) process CIBC VISA transactions file: {}".format(cibc_csv_visa))
+def process_cibc_visa(input_file):
+    print("\n3) process CIBC VISA transactions file: {}".format(input_file))
     wb = load_workbook(output_file)
     global cibc_csv_visa_sheet_name
     cibc_csv_visa_sheet_name = tran_mon + "-cibc visa"
     ws = wb.create_sheet(cibc_csv_visa_sheet_name)
 
     # copy from csv to output_file
-    with open(cibc_csv_chq,'r') as f:
+    with open(input_file,'r') as f:
         for row in csv.reader(f):
             ws.append(row)
 
     # format column 3 & 4 currency + add transaction type
     i = 1
     while i <= ws.max_row:
+
+        if ws.cell(row=i, column=3).value is "":
+            ws.cell(row=i, column=3).value = 0
+        
+        if ws.cell(row=i, column=4).value is "":
+            ws.cell(row=i, column=4).value = 0
+
+        ws.cell(row=i, column=3).value = float(ws.cell(row=i, column=3).value)
+        ws.cell(row=i, column=4).value = float(ws.cell(row=i, column=4).value)
+
         ws.cell(row=i, column=3).number_format = numbers.FORMAT_CURRENCY_USD_SIMPLE
         ws.cell(row=i, column=4).number_format = numbers.FORMAT_CURRENCY_USD_SIMPLE
+        
         ws.cell(row=i, column=5).value = 'CIBC VISA'
         i +=1
 
@@ -214,15 +232,54 @@ sample template process function
 """###################################################
 ###################################################"""
 def construct_monthly_transactions():
-    print("abc")
+    print("\n4) construct {} sheet in {}".format(output_file_sheet_title,output_file))
+    wb = load_workbook(output_file)
+    ws_dst = wb.create_sheet(output_file_sheet_title)
+    dst_row_cnt = 0
 
+    # read from bmo csv file
+    ws_src1 = wb[bmo_csv_mc_sheet_name]
+    maxr = ws_src1.max_row
+    maxc = ws_src1.max_column
+    for r in range(1, maxr+1):
+        for c in range(1, maxc+1):
+            ws_dst.cell(row=(r+dst_row_cnt),column=c).value = ws_src1.cell(row=r,column=c).value
+
+    # read from cibc-chq file
+    dst_row_cnt = dst_row_cnt + maxr
+    ws_src2 = wb[cibc_csv_chq_sheet_name]
+    maxr = ws_src2.max_row
+    maxc = ws_src2.max_column
+    for r in range(1, maxr+1):
+        for c in range(1, maxc+1):
+            ws_dst.cell(row=(r+dst_row_cnt),column=c).value = ws_src2.cell(row=r,column=c).value
+
+    # read from cibc-visa file
+    dst_row_cnt = dst_row_cnt + maxr
+    ws_src3 = wb[cibc_csv_visa_sheet_name]
+    maxr = ws_src3.max_row
+    maxc = ws_src3.max_column
+    for r in range(1, maxr+1):
+        for c in range(1, maxc+1):
+            ws_dst.cell(row=(r+dst_row_cnt),column=c).value = ws_src3.cell(row=r,column=c).value
+
+    # re-format column A, C and D since format may get corrupted in inter-sheet copying
+    i = 1
+    while i <= ws_dst.max_row:
+        ws_dst.cell(row=i, column=1).number_format = numbers.FORMAT_DATE_YYYYMMDD2
+        ws_dst.cell(row=i, column=3).number_format = numbers.FORMAT_CURRENCY_USD_SIMPLE
+        ws_dst.cell(row=i, column=4).number_format = numbers.FORMAT_CURRENCY_USD_SIMPLE        
+        i +=1
+
+    wb.save(output_file)
+    print("done")
 
 def main():
     process_user_input()
     process_bmo_mc(bmo_csv_mc)
     process_cibc_chq(cibc_csv_chq)
     process_cibc_visa(cibc_csv_visa)
-    construct_monthly_transactions
+    construct_monthly_transactions()
 
 if __name__== "__main__":
   main()
